574f671dcc1a6e89c04070a7c585892188de2d22
dartweider2000 <nikita.ivanov574477333@gmail.com>
2023-08-23 13:45:02 +0300

    header is made

--- src/js/app.js
+++ src/js/app.js
@@ -24,3 +24,60 @@ burgerMenu(); //Запуск бургера
 //adaptive() //Запускаю динамический адаптив
 
 [...document.links].forEach(link => link.addEventListener('click', e => e.preventDefault()));
+
+const scrolIsUp = () => {
+   let scroll = scrollY;
+   const check = () => {
+      let scrollNow = scrollY;
+      let delta = scroll - scrollNow;
+      scroll = scrollNow;
+      return delta > 0;
+   }
+   return check;
+};
+
+// const scr = scrolIsUp();
+
+// document.addEventListener('scroll', () => console.log(scr()));
+
+const init = () => {
+   if(!document.querySelector('[data-header-change]'))
+      return;
+
+   const header = document.querySelector('[data-header-change]');
+   const headerStartChangeSelector = header.dataset.headerChange;
+   const changeDirectionElement = document.querySelector(headerStartChangeSelector);
+   let changeStart = changeDirectionElement.offsetTop;
+   const scroll = scrolIsUp();
+   const windowScrollHandler = () => {
+      const scrollValue = scroll();
+      if(!scrollValue){
+         if(scrollY + header.offsetHeight >= changeStart){
+            header.classList.add('_change');
+         }
+      }else if(scrollValue){
+         if(scrollY + header.offsetHeight < changeStart){
+            header.classList.remove('_change');
+         }
+      }
+      changeStart = changeDirectionElement.offsetTop;
+   };
+   // const windowHeaderLittleBigResizeHandler = () => {
+   //    changeStart = changeDirectionElement.offsetTop;
+   //    if(window.innerWidth < 768){
+   //       header.classList.remove('little-big');
+   //    }else{
+   //       if(pageYOffset + header.offsetHeight >= changeStart){
+   //          header.classList.add('little-big');
+   //       }
+   //    }
+   // };
+   // windowHeaderLittleBigResizeHandler();
+   // window.addEventListener('resize', () => {
+   //    setTimeout(() => windowHeaderLittleBigResizeHandler, 300);
+
+   // });
+   window.addEventListener('scroll', windowScrollHandler);
+};
+
+init();
